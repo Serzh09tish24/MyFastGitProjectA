9cbc572863e67536a5a5b11d80029bf691a7002c
Sergey Tish <zen1925it@yandex.ru>
2023-10-28 16:10:26 +0300

    Second Commit

--- file3.cpp
+++ file3.cpp
@@ -2,6 +2,12 @@
 
 int main(int argc, char*argv[])
 {
-    std::cout<<"Hello World"<<std::endl;
+    int n = 10;
+    for(size_t i{0}; i != n; ++i)
+    {
+        std::cout<<"count["<<i<<"] = "<<i<<std::endl;
+    }
+
+    std::cout<<"Hello World!!!!"<<std::endl;
     return 0;
 }
